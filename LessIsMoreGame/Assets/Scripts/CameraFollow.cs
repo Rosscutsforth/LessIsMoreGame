b16d25ee21f06831078fe90f0716867bb2ce2927
Rosscutsforth <Rosscutsforth@gmail.com>
2021-01-24 00:58:52 -0800

    Basic Character Controls
    Made a large ground mesh and created basic movement and jumping for the character.

---
+++ LessIsMoreGame/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float speed;
+
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    private void Start()
+    {
+        transform.position = transform.position;
+    }
+
+    private void Update()
+    {
+        if (playerTransform != null)
+        {
+
+            float clampedX = Mathf.Clamp(playerTransform.position.x, minX, maxX);
+            float clampedY = Mathf.Clamp(playerTransform.position.y, minY, maxY);
+
+            transform.position = Vector3.Lerp(transform.position, new Vector3(playerTransform.position.x, playerTransform.position.y + 7f, playerTransform.position.z - 15f), speed);
+        }
+    }
+}
---
+++ LessIsMoreGame/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody playerRB;
+    private Animator playerAnim;
+    public LayerMask groundLayer;
+    private CapsuleCollider capsu;
+
+    //Declaring variables for player movement
+    private Vector3 moveAmount;
+
+    //Declaring variables for player jumping
+    public float jumpForce;
+    public float distanceToGround = 0.1f;
+
+    private void Start()
+    {
+        playerAnim = GetComponent<Animator>();
+        playerRB = GetComponent<Rigidbody>();
+
+        capsu = GetComponent<CapsuleCollider>();
+    }
+
+    private void Update()
+    {
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        moveAmount = moveInput.normalized * speed;
+
+        if (IsGrounded() && Input.GetButtonDown("Jump"))
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        playerRB.MovePosition(playerRB.position + moveAmount * Time.fixedDeltaTime);
+    }
+
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(capsu.bounds.center.x, capsu.bounds.min.y,
+        capsu.bounds.center.z);
+
+        bool grounded = Physics.CheckCapsule(capsu.bounds.center, capsuleBottom, distanceToGround,
+        groundLayer, QueryTriggerInteraction.Ignore);
+
+        return grounded;
+    }
+
+}
